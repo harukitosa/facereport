8a4e153608b5bc7384822750a80512806c820686
harukitosa <13haruki28@gmail.com>
2020-07-28 12:02:27 +0900

    fix

--- adaboost.cpp
+++ adaboost.cpp
@@ -128,7 +128,7 @@ void adaboost(cv::Mat* images, int* labels, int sample_num)
       for (int k = 0;k < sample_num;k++) {
         if (min_error[k] != 0) {
           w[k] = w[k]*min_error[k]/(1-min_error[k]);
-          if (k = 0){
+          if (k == 0){
             std::cout << "w[" << k << "]" << w[k] << std::endl;
           }
         }
